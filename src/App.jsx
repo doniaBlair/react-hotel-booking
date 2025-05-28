e982901a88d41f9c97258f4e203dbb15eaaa4214
Donia Blair <donia.blair@gmail.com>
2025-05-28 14:03:25 -0500

    owner pages complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
+import HotelRegistration from "./components/HotelRegistration";
+import Layout from "./pages/hotel-owner/Layout";
+import Dashboard from "./pages/hotel-owner/Dashboard";
+import AddRoom from "./pages/hotel-owner/AddRoom";
+import ListRoom from "./pages/hotel-owner/ListRoom";
 
 const App = () => {
     const isOwnerPath = useLocation().pathname.includes('owner');
@@ -12,11 +18,18 @@ const App = () => {
 	return (
 		<>
 			{!isOwnerPath && <Navbar />}
+            {false && <HotelRegistration />}
             <div className="min-h-[70vh]">
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/rooms" element={<AllRooms />} />
                     <Route path="/rooms/:id" element={<RoomDetails />} />
+                    <Route path="/my-bookings" element={<MyBookings />} />
+                    <Route path='/owner' element={<Layout />}>
+                        <Route index element={<Dashboard />} />
+                        <Route path="add-room" element={<AddRoom />} />
+                        <Route path="list-room" element={<ListRoom />} />
+                    </Route>
                 </Routes>
             </div>
             <Footer />
